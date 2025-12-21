71b3ba6342a7a371e4c24879db3b0003e6b868a1
Obuidul Islam Pias <obuidul.pias@gmail.com>
2025-12-21 21:04:14 +0600

    Add search for permission without loading

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -36,10 +36,6 @@
     <div class="wrapper">
 
         <!-- Preloader -->
-        <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="{{ asset('admin/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo"
-                height="60" width="60">
-        </div>
 
         <!-- Navbar -->
         @include('partials.navbar')
@@ -106,6 +102,8 @@
     <script src="{{ asset('admin/dist/js/demo.js') }}"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <script src="{{ asset('admin/dist/js/pages/dashboard.js') }}"></script>
+    
+    @yield('scripts')
 </body>
 
 </html>
